85b0aa8fdf85e7d79b0c05181a9cee6a706ac9c0
shashank9487 <shashank9487@gmail.com>
2015-02-03 16:03:16 +0530

    edit edit.php controller

--- catalog/language/english/account/edit.php
+++ catalog/language/english/account/edit.php
@@ -19,6 +19,7 @@ $_['entry_fax']          = 'Fax';
 $_['error_exists']       = 'Warning: E-Mail address is already registered!';
 $_['error_firstname']    = 'First Name must be between 1 and 32 characters!';
 $_['error_lastname']     = 'Last Name must be between 1 and 32 characters!';
+$_['error_name_type']    = 'Name contains only characters';
 $_['error_email']        = 'E-Mail Address does not appear to be valid!';
 $_['error_telephone']    = 'Telephone must be between 3 and 32 characters!';
 $_['error_custom_field'] = '%s required!';
